6eac36f131a9b3cc1710ccc75c0b6308541ea39c
ShcIZZiKK <ShcIZZO@mail.ru>
2023-01-07 20:04:07 +0700

    add auto-deploy

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,23 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
+process.env.NODE_ENV === "production" ? "/dynamometer/" : "/",
+  (module.exports = defineConfig({
+    transpileDependencies: true,
 
-  pluginOptions: {
-    svgSprite: {
-      dir: "src/assets/svg",
-      test: /\.(svg)(\?.*)?$/,
-      loaderOptions: {
-        extract: true,
-        spriteFilename: "img/icons.[hash:8].svg",
-      },
-      pluginOptions: {
-        plainSprite: true,
+    pluginOptions: {
+      svgSprite: {
+        dir: "src/assets/svg",
+        test: /\.(svg)(\?.*)?$/,
+        loaderOptions: {
+          extract: true,
+          spriteFilename: "img/icons.[hash:8].svg",
+        },
+        pluginOptions: {
+          plainSprite: true,
+        },
       },
     },
-  },
 
-  chainWebpack: (config) => {
-    config.module.rule("svg-sprite").use("svgo-loader").loader("svgo-loader");
-  },
-});
+    chainWebpack: (config) => {
+      config.module.rule("svg-sprite").use("svgo-loader").loader("svgo-loader");
+    },
+  }));
